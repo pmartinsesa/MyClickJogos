cf7539da6de92b85b6f2ab2f8fc87330ae3fa917
pmes18 <pmartinsesa@gmail.com>
2021-07-07 14:19:53 -0300

    Snake eat and dead

--- src/app/snake-game/snake-game.component.ts
+++ src/app/snake-game/snake-game.component.ts
@@ -6,19 +6,50 @@ import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
   styleUrls: ['./snake-game.component.css']
 })
 export class SnakeGameComponent implements OnInit {
+  private direction: number = 0;
+  private initializeGame: boolean = false;
   private snakeBody = [
     {x: 10, y: 11},
     {x: 11, y: 11},
-    {x: 12, y: 11}
+    {x: 12, y: 11},
   ]
-
-  // private snake!: HTMLDivElement;
-  private fruit!: HTMLDivElement;
+  private fruit: any = {
+    fruitElement: HTMLDivElement,
+    position: {
+      x: 2,
+      y: 2
+    },
+    isActive: false
+  }
 
   @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
-    console.log(event.key);
-    // this.SnakeWalk();
+    const key = event.key;
+    console.log(key);
+    switch(key)
+    {
+      case 'w':
+        this.initializeGame = true;
+        this.direction = 0
+        break;
+      case 'a':
+        this.initializeGame = true;
+        this.direction = 3
+        break;
+      case 's':
+        this.initializeGame = true;
+        this.direction = 2
+        break;
+      case 'd':
+        this.initializeGame = true;
+        this.direction = 1
+        break;
+      case 'p':
+        this.initializeGame = false;
+        break;
+      default:
+        break;
+    }
   }
 
   constructor(
@@ -26,25 +57,68 @@ export class SnakeGameComponent implements OnInit {
     private renderer: Renderer2
     ) {}
 
+  private eatFruit(tail: any) {
+    if (this.fruit.isActive) {
+      console.log(this.fruit.isActive);
+      this.snakeBody.push({x: tail.x + 1, y: tail.y});
+      this.fruit.isActive = false;
+    }
+
+    const fruit = this.elementRef.nativeElement.querySelector('.fruit');
+
+    console.log(fruit);
+    this.renderer.removeChild(this.elementRef.nativeElement.querySelector('#table'), fruit);
+  }
+
+  private dead() {
+    console.log('f');
+    this.initializeGame = false;
+  }
+
+  private analyzeWhatTheSnakeEats(): boolean {
+    let eatYourself: boolean = false
+
+    for (let i = 1; i < this.snakeBody.length; i++) {
+      if (this.snakeBody[0].x === this.snakeBody[i].x && this.snakeBody[0].y === this.snakeBody[i].y) {
+        eatYourself = true;
+        break;
+      }
+    }
+
+    return eatYourself;
+  }
+
   ngOnInit(): void {
 
     setInterval(() => {
-      this.initializeTable();
-      this.SnakeWalk();
-    }, 500);
+      if (this.initializeGame) {
+        this.renderTable();
+        this.SnakeWalk(this.direction);
+
+        let eatAFruit = this.snakeBody[0].x === this.fruit.position.x && this.snakeBody[0].y === this.fruit.position.y;
+        let eatYourself = this.analyzeWhatTheSnakeEats();
+        if (eatAFruit){
+          this.eatFruit(this.snakeBody[this.snakeBody.length - 1]);
+        }
+        else if (eatYourself) {
+          this.dead();
+        }
+      }
+    }, 180);
   }
 
   ngDoCheck(): void {
     // this.SnakeWalk();
   }
 
-  private initializeTable() {
+  private renderTable() {
     const oldSnakeBody = this.elementRef.nativeElement.querySelectorAll('.snake');
 
     oldSnakeBody.forEach((element: any) => {
       this.renderer.removeChild(this.elementRef.nativeElement.querySelector('#table'), element);
     });
 
+    // console.log(this.snakeBody);
     this.snakeBody.forEach((element, index) => {
       const snake = this.renderer.createElement('div');
       this.renderer.addClass(snake, 'snake');
@@ -53,20 +127,66 @@ export class SnakeGameComponent implements OnInit {
       this.renderer.appendChild(this.elementRef.nativeElement.querySelector('#table'), snake);
     })
 
-    console.log(this.snakeBody);
-    // this.fruit = this.renderer.createElement('div');
-    // this.renderer.addClass(this.fruit, 'fruit');
+    // console.log(this.fruit.isActive);
+    if (!this.fruit.isActive) {
+      this.fruit.fruitElement = this.renderer.createElement('div');
+      // console.log(this.fruit.fruitElement )
 
-    // this.renderer.appendChild(this.elementRef.nativeElement.querySelector('#table'), this.fruit);
+      this.fruit.position.x = Math.floor(Math.random() * 21);
+      this.fruit.position.y = Math.floor(Math.random() * 21);
+
+      this.renderer.addClass(this.fruit.fruitElement, 'fruit');
+      this.fruit.fruitElement.style.gridRowStart = this.fruit.position.x;
+      this.fruit.fruitElement.style.gridColumnStart = this.fruit.position.y;
+      this.renderer.appendChild(this.elementRef.nativeElement.querySelector('#table'), this.fruit.fruitElement);
+      this.fruit.isActive = true;
+    }
+
   }
 
-  private SnakeWalk() {
+  // 0 -> frente
+  // 1 -> direita
+  // 2 -> baixo
+  // 3 -> esquerda
+  private SnakeWalk(direction: number) {
     for(let i = this.snakeBody.length - 2; i >= 0; i--) {
       this.snakeBody[i+1] = { ...this.snakeBody[i]}
     }
 
-    this.snakeBody[0].x --;
-    // console.log(this.snakeBody)
+    switch(direction) {
+      case 0:
+        if (this.snakeBody[0].x - 1 < 0) {
+          this.snakeBody[0].x = 21;
+        }
+        else {
+          this.snakeBody[0].x --;
+        }
+        break;
+      case 1:
+        if (this.snakeBody[0].y + 1 > 21) {
+          this.snakeBody[0].y = 0;
+        }
+        else {
+          this.snakeBody[0].y ++;
+        }
+        break;
+      case 2:
+        if (this.snakeBody[0].x + 1 > 21) {
+          this.snakeBody[0].x = 0;
+        }
+        else {
+          this.snakeBody[0].x ++;
+        }
+        break;
+      case 3:
+        if (this.snakeBody[0].y - 1 < 0) {
+          this.snakeBody[0].y = 21;
+        }
+        else {
+          this.snakeBody[0].y --;
+        }
+        break;
+    }
   }
 
 }
